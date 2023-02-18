d13d15a9a009dea191dc955741e2acd9132f9c5c
Camila Pinzon <camilapinz18@gmail.com>
2023-02-18 11:58:36 -0500

    port

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require('express')
 require('./db')
 const app = express()
 app.use(express.json())
+require('dotenv').config();
 
-const PORT = 3000
+const PORT = process.env.PORT
+
 
 app.get('/', (request, response) => {
   response.json('Connected lo local server')
