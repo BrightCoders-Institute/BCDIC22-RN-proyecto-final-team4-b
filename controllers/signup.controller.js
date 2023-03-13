2cbf3a2f81f14ec569fba877e041c0f7ec1e5c71
Camila Pinzon <camilapinz18@gmail.com>
2023-03-13 12:55:29 -0500

    Dependencies added
    Bcrypt
    Express-validator

--- controllers/signup.controller.js
+++ controllers/signup.controller.js
@@ -1,18 +1,49 @@
 const db = require('../db/db')
-const { validateCreate } = require('../validators/signupValidator')
 const bcryptjs = require('bcryptjs')
 
+let createAccountScreenInfo = null
+
+const createAccount = (request, response) => {
+  const { user_name, date, partner_name } = request.body
+  console.log('Estoy en account back', user_name, partner_name, date)
+  createAccountScreenInfo = {
+    user_name: user_name,
+    partner_name: partner_name,
+    date: date
+  }
+
+  if (user_name && partner_name && date) {
+    response.status(200).send({ message: 'Data saved' })
+  } else {
+    response.status(400).send({ message: 'Please fill all the fields to continue' })
+  }
+
+  console.log('createAccountScreenInfo', createAccountScreenInfo)
+}
+
 const createUser = async (req, res) => {
-  const { user_name, password, confirmPassword, email, date, partner_name } =
-    req.body
+  const { password, confirmPassword, email } = req.body
+  console.log(
+    'estoy en signup back',
+
+    'passwrod',
+    password,
+    'passwrodConfirm',
+    confirmPassword,
+    'email',
+    email
+  )
+
   const [alreadyExists] = await db.query('SELECT * FROM User WHERE email=?', [
     email
   ])
+
   if (alreadyExists.length) {
     res.status(409).send({ message: 'User already exists' })
   } else {
     //encriptar clave:
     if (password === confirmPassword) {
+      console.log('E NOPXISTS____', alreadyExists)
       try {
         const salt = await bcryptjs.genSalt(10)
         const hashedPassword = await bcryptjs.hash(password, salt)
@@ -20,13 +51,14 @@ const createUser = async (req, res) => {
           'INSERT INTO User (user_name, password, email, date, partner_name) VALUES(?,?,?,?,?)',
           [user_name, hashedPassword, email, date, partner_name]
         )
-        res.send({
+        res.status(200).send({
           id: rows.insertId,
           user_name,
           hashedPassword,
           email,
           date,
-          partner_name
+          partner_name,
+          message: 'User created successfully'
         })
       } catch (error) {
         console.error(error)
@@ -41,5 +73,6 @@ const createUser = async (req, res) => {
 }
 
 module.exports = {
+  createAccount,
   createUser
 }
--- routes/signup.routes.js
+++ routes/signup.routes.js
@@ -4,5 +4,6 @@ const controller = require('../controllers/signup.controller')
 const { validateCreate } = require('../validators/signupValidator')
 
 router.post('/signup', validateCreate, controller.createUser)
+router.post('/create',controller.createAccount)
 
 module.exports = router
