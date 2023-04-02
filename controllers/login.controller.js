d8c3f24005686e0651f07ebfd1538a52f0a03c88
Camila Pinzon <camilapinz18@gmail.com>
2023-04-02 17:49:28 -0500

    login changed to mongoose

--- controllers/login.controller.js
+++ controllers/login.controller.js
@@ -5,10 +5,11 @@ const User=require('../models/user')
 const loginUser = async (request, response) => {
   const { password, email } = request.body
 
-  const userExists=await User.findOne({ identification })
+  const userExists=await User.findOne({ email })
 
-  if (userExists.length) {
-    const isMatch = await bcryptjs.compare(password, userExists[0].password)
+  if (userExists) {
+    console.log("PASSWORD",userExists)
+    const isMatch = await bcryptjs.compare(password, userExists.hashedPassword)
     if (isMatch) {
       response.status(200).send({ message: 'Successful login' ,userExists})
     } else {
